261a7c5e17c5854b2d9e40ed62f5f91f87c749e5
LA FONTAINE DEL CAMPO <mlafo3@eq.edu.au>
2018-05-03 18:27:46 +1000

    nesesito ayuda en esto, aser el malo

--- app/main.js
+++ app/main.js
@@ -48,4 +48,66 @@ function arrowRight() {
   console.log('arrowRight', hero.offsetLeft);
   hero.style.left = (hero.offsetLeft + 3) + 'px';
 
-} 
+}
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function () {
+  // console.log('ready');
+  document.addEventListener('keydown', function (key) {
+    // console.log('key', key);
+    switch (key.key) {
+      case W: // key Up
+        Up();
+        break;
+
+      case s: // key Down
+        Down();
+        break;
+
+      case a: // key left
+        Left();
+        break;
+
+      case d: // key Right
+        Right();
+        break;
+
+    }
+  });
+};
+
+function Up() {
+  var villain = document.getElementById('villain');
+  console.log('Up', villain.offsetTop);
+  villain.style.top = (villain.offsetTop - 3) + 'px';
+}
+
+function Down() {
+  var villain = document.getElementById('vilain');
+  console.log('Down', villain.offsetTop);
+  villain.style.top = (villain.offsetTop + 3) + 'px';
+}
+
+function Left() {
+  var villain = document.getElementById('villain');
+  console.log('left', villain.offsetLeft);
+  villain.style.left = (villain.offsetLeft - 3) + 'px';
+}
+
+function Right() {
+  var villain = document.getElementById('villain');
+  console.log('Right', villain.offsetLeft);
+  villain.style.left = (villain.offsetLeft + 3) + 'px';
+
+}
+
+
