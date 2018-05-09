a5a99ac65ed1eaad8188333db10016d5e24c50b0
Ernesto La Fontaine <pajarraco@sharon.home>
2018-05-09 19:42:13 +1000

    new movements

--- app/main.js
+++ app/main.js
@@ -1,94 +1,160 @@
 'use strict';
 
-window.onload = function () {
-  // console.log('ready');
-  document.addEventListener('keydown', function (key) {
-    // console.log('key', key);
+window.onload = function() {
+  var keyUpIsPress = false;
+  var keyDownIsPress = false;
+  var keyLeftIsPress = false;
+  var keyRightIsPress = false;
+
+  var isJumping = null;
+  var isCrouching = null;
+  var isMovingLeft = null;
+  var isMovingRight = null;
+
+  var hero = document.getElementById('hero');
+
+  document.addEventListener('keydown', function(key) {
     switch (key.keyCode) {
       case 38: // key ArrowUp
-        arrowUp();
+        arrowUpDown();
         break;
 
       case 40: // key ArrowDown
-        arrowDown();
+        arrowDownDown();
         break;
 
       case 37: // key Arrowleft
-        arrowLeft();
+        arrowLeftDown();
         break;
 
       case 39: // key ArrowRight
-        arrowRight();
-        break;
-
-      case 87: // key Up
-        Up();
-        break;
-
-      case 83: // key Down
-        Down();
-        break;
-
-      case 65: // key left
-        Left();
-        break;
-
-      case 68: // key Right
-        Right();
+        arrowRightDown();
         break;
     }
   });
+
+  document.addEventListener('keyup', function(key) {
+    switch (key.keyCode) {
+      case 38: // key ArrowUp
+        arrowUpUp();
+        break;
+
+      case 40: // key ArrowDown
+        arrowDownUp();
+        break;
+
+      case 37: // key Arrowleft
+        arrowLeftUp();
+        break;
+
+      case 39: // key ArrowRight
+        arrowRightUp();
+        break;
+    }
+  });
+
+  // key actions
+  function arrowUpDown() {
+    if (!keyUpIsPress) {
+      console.log('start jump');
+
+      moveJump();
+      keyUpIsPress = true;
+    }
+  }
+
+  function arrowDownDown() {
+    if (!keyDownIsPress) {
+      console.log('start crouch');
+      moveCrouch();
+      keyDownIsPress = true;
+    }
+  }
+
+  function arrowLeftDown() {
+    if (!keyLeftIsPress) {
+      console.log('start left');
+      moveLeft();
+      keyLeftIsPress = true;
+    }
+  }
+
+  function arrowRightDown() {
+    if (!keyRightIsPress) {
+      console.log('start right');
+      moveRight();
+      keyRightIsPress = true;
+    }
+  }
+
+  function arrowUpUp() {
+    if (keyUpIsPress) {
+      console.log('stop jump');
+      stopJump();
+      keyUpIsPress = false;
+    }
+  }
+
+  function arrowDownUp() {
+    if (keyDownIsPress) {
+      console.log('stop crouch');
+      stopCrouch();
+      keyDownIsPress = false;
+    }
+  }
+
+  function arrowLeftUp() {
+    if (keyLeftIsPress) {
+      console.log('stop left');
+      keyLeftIsPress = false;
+    }
+  }
+
+  function arrowRightUp() {
+    if (keyRightIsPress) {
+      console.log('stop right');
+      keyRightIsPress = false;
+    }
+  }
+
+  // movements
+  function moveJump() {
+    isJumping = setInterval(function() {
+      if (hero.offsetTop > 0) {
+        hero.style.top = hero.offsetTop - 1 + 'px';
+      } else {
+        clearInterval(isJumping);
+      }
+    }, 5);
+  }
+
+  function moveCrouch() {
+    isCrouching = setInterval(function() {
+      if (hero.offsetTop < window.innerHeight - hero.offsetHeight) {
+        hero.style.top = hero.offsetTop + 1 + 'px';
+      } else {
+        clearInterval(isCrouching);
+      }
+    }, 5);
+  }
+
+  function moveLeft() {
+    hero.style.left = hero.offsetLeft - 1 + 'px';
+  }
+
+  function moveRight() {
+    hero.style.left = hero.offsetLeft + 1 + 'px';
+  }
+
+  function stopJump() {
+    clearInterval(isJumping);
+  }
+
+  function stopCrouch() {
+    clearInterval(isCrouching);
+  }
+
+  function stopMoveLeft() {}
+
+  function stopMoveRight() {}
 };
-
-function arrowUp() {
-  var hero = document.getElementById('hero');
-  console.log('arrowUp', hero.offsetTop);
-  hero.style.top = (hero.offsetTop - 3) + 'px';
-}
-
-function arrowDown() {
-  var hero = document.getElementById('hero');
-  console.log('arrowDown', hero.offsetTop);
-  hero.style.top = (hero.offsetTop + 3) + 'px';
-}
-
-function arrowLeft() {
-  var hero = document.getElementById('hero');
-  console.log('arrowleft', hero.offsetLeft);
-  hero.style.left = (hero.offsetLeft - 3) + 'px';
-}
-
-function arrowRight() {
-  var hero = document.getElementById('hero');
-  console.log('arrowRight', hero.offsetLeft);
-  hero.style.left = (hero.offsetLeft + 3) + 'px';
-
-}
-
-function Up() {
-  var villain = document.getElementById('villain');
-  console.log('Up', villain.offsetTop);
-  villain.style.top = (villain.offsetTop - 3) + 'px';
-}
-
-function Down() {
-  var villain = document.getElementById('villain');
-  console.log('Down', villain.offsetTop);
-  villain.style.top = (villain.offsetTop + 3) + 'px';
-}
-
-function Left() {
-  var villain = document.getElementById('villain');
-  console.log('left', villain.offsetLeft);
-  villain.style.left = (villain.offsetLeft - 3) + 'px';
-}
-
-function Right() {
-  var villain = document.getElementById('villain');
-  console.log('Right', villain.offsetLeft);
-  villain.style.left = (villain.offsetLeft + 3) + 'px';
-
-}
-
-
-
