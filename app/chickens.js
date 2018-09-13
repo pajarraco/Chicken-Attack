80b22e40fa764d65021c1f06c8d9a12a4e0494d3
LA FONTAINE DEL CAMPO <mlafo3@eq.edu.au>
2018-09-13 20:24:28 +1000

    backrounds

--- app/chickens.js
+++ app/chickens.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
 function defaultChicken() {
   return {
-    id: '',
-    type: 'chicken',
+    id: "",
+    type: "chicken",
     movement: null,
-    state: ['movingLeft'],
-    img: 'assets/guard-left.png',
+    state: ["movingLeft"],
+    img: "assets/guard-left.png",
     images: {
-      base: 'assets/guard-still.png',
-      left: 'assets/guard-left.png',
-      up: '',
-      down: ''
+      base: "assets/guard-still.png",
+      left: "assets/guard-left.png",
+      up: "",
+      down: ""
     }
   };
 }
 
 function createChicken(id) {
-  var peopleComponent = document.getElementById('people');
-  var chicken = document.createElement('div');
-  var chickenImg = document.createElement('img');
+  var peopleComponent = document.getElementById("people");
+  var chicken = document.createElement("div");
+  var chickenImg = document.createElement("img");
 
-  chicken.setAttribute('id', id);
-  chicken.className = 'chicken';
+  chicken.setAttribute("id", id);
+  chicken.className = "chicken";
   chicken.appendChild(chickenImg);
   peopleComponent.appendChild(chicken);
 }
@@ -32,7 +32,7 @@ function loadChicken() {
   chicken.id = createID();
   createChicken(chicken.id);
   var component = document.getElementById(chicken.id);
-  component.style.left = window.innerWidth + 'px';
+  component.style.left = window.innerWidth + "px";
   var img = component.children;
   img[0].scr = chicken.img;
   chicken.top = component.offsetTop;
@@ -44,13 +44,13 @@ function loadChicken() {
 
 function chickenMomevement(chicken) {
   if (chicken.left <= 0) {
-    var i = chicken.state.indexOf('movingLeft');
+    var i = chicken.state.indexOf("movingLeft");
     chicken.state.splice(i, 1);
-    chicken.state.push('movingRight');
+    chicken.state.push("movingRight");
   } else if (chicken.left + chicken.width >= window.innerWidth) {
-    var i = chicken.state.indexOf('movingRight');
+    var i = chicken.state.indexOf("movingRight");
     chicken.state.splice(i, 1);
-    chicken.state.push('movingLeft');
+    chicken.state.push("movingLeft");
   }
   return chicken;
 }
@@ -59,5 +59,5 @@ function createID() {
   var S4 = function() {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
-  return S4() + S4() + '-' + S4() + '-' + S4() + S4() + S4();
+  return S4() + S4() + "-" + S4() + "-" + S4() + S4() + S4();
 }
